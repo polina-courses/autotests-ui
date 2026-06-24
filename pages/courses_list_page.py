85da55599b2c9dafdca68fe939a930b43e67fc05
sabir <sabir@dominion.ru>
2026-06-24 16:11:20 +0400

    Практикуемся в реализации PageComponent. Практикуемся в написании компонентов с использованием паттерна PageComponent

--- pages/courses_list_page.py
+++ pages/courses_list_page.py
@@ -1,7 +1,10 @@
-from playwright.sync_api import Page, expect
+from playwright.sync_api import Page
 
+from components.courses.course_view_component import CourseViewComponent
+from components.courses.courses_list_toolbar_view_component import CoursesListToolbarViewComponent
 from components.navigation.navbar_component import NavbarComponent
 from components.navigation.sidebar_component import SidebarComponent
+from components.views.empty_view_component import EmptyViewComponent
 from pages.base_page import BasePage
 
 
@@ -11,47 +14,13 @@ class CoursesListPage(BasePage):
 
         self.navbar = NavbarComponent(page)
         self.sidebar = SidebarComponent(page)
-
-        self.courses_title = page.get_by_test_id('courses-list-toolbar-title-text')
-        self.create_course_button = page.get_by_test_id('courses-list-toolbar-create-course-button')
-        self.course_title = page.get_by_test_id('course-widget-title-text')
-        self.course_image = page.get_by_test_id('course-preview-image')
-        self.course_max_score_text = page.get_by_test_id('course-max-score-info-row-view-text')
-        self.course_min_score_text = page.get_by_test_id('course-min-score-info-row-view-text')
-        self.course_estimated_time_text = page.get_by_test_id('course-estimated-time-info-row-view-text')
-        self.empty_view_icon = page.get_by_test_id('courses-list-empty-view-icon')
-        self.empty_view_title = page.get_by_test_id('courses-list-empty-view-title-text')
-        self.empty_view_description = page.get_by_test_id('courses-list-empty-view-description-text')
-
-
-    def check_visible_courses_title(self):
-        expect(self.courses_title).to_be_visible()
-        expect(self.courses_title).to_have_text('Courses')
-
-
-    def check_visible_create_course_button(self):
-        expect(self.create_course_button).to_be_visible()
+        self.empty_view = EmptyViewComponent(page, 'courses-list')
+        self.course_view = CourseViewComponent(page)
+        self.toolbar_view = CoursesListToolbarViewComponent(page)
 
 
     def check_visible_empty_view(self):
-        expect(self.empty_view_icon).to_be_visible()
-
-        expect(self.empty_view_title).to_be_visible()
-        expect(self.empty_view_title).to_have_text('There is no results')
-
-        expect(self.empty_view_description).to_be_visible()
-        expect(self.empty_view_description).to_have_text('Results from the load test pipeline will be displayed here')
-
-
-    def check_visible_course_card(self, index: int, title: str, max_score: str, min_score: str, estimated_time: str):
-        expect(self.course_image.nth(index)).to_be_visible()
-        fields = [
-            (self.course_title, title),
-            (self.course_max_score_text, f"Max score: {max_score}"),
-            (self.course_min_score_text, f"Min score: {min_score}"),
-            (self.course_estimated_time_text, f"Estimated time: {estimated_time}"),
-        ]
-        for locator, expected_text in fields:
-            element = locator.nth(index)
-            expect(element).to_be_visible()
-            expect(element).to_have_text(expected_text)
+        self.empty_view.check_visible(
+            title='There is no results',
+            description='Results from the load test pipeline will be displayed here'
+        )
--- pages/create_course_page.py
+++ pages/create_course_page.py
@@ -1,108 +1,31 @@
-from playwright.sync_api import Page, expect
+from playwright.sync_api import Page
+
+from components.courses.create_course_exercise_form_component import CreateCourseExerciseFormComponent
+from components.courses.create_course_exercises_toolbar_view_component import (
+    CreateCourseExercisesToolbarViewComponent,
+)
+from components.courses.create_course_form_component import CreateCourseFormComponent
+from components.courses.create_course_toolbar_view_component import CreateCourseToolbarViewComponent
+from components.navigation.navbar_component import NavbarComponent
+from components.views.empty_view_component import EmptyViewComponent
+from components.views.image_upload_widget_component import ImageUploadWidgetComponent
 from pages.base_page import BasePage
+
 
 class CreateCoursePage(BasePage):
     def __init__(self, page: Page):
         super().__init__(page)
 
-        self.create_course_title = page.get_by_test_id('create-course-toolbar-title-text')
-        self.create_course_button = page.get_by_test_id('create-course-toolbar-create-course-button')
-        self.preview_empty_view_icon = page.get_by_test_id('create-course-preview-empty-view-icon')
-        self.preview_empty_view_title = page.get_by_test_id('create-course-preview-empty-view-title-text')
-        self.preview_empty_view_description = page.get_by_test_id('create-course-preview-empty-view-description-text')
-        self.preview_image_upload_icon = page.get_by_test_id('create-course-preview-image-upload-widget-info-icon')
-        self.preview_image_upload_title = page.get_by_test_id('create-course-preview-image-upload-widget-info-title-text')
-        self.preview_image_upload_description = page.get_by_test_id('create-course-preview-image-upload-widget-info-description-text')
-        self.preview_image_upload_button = page.get_by_test_id('create-course-preview-image-upload-widget-upload-button')
-        self.preview_image_remove_button = page.get_by_test_id('create-course-preview-image-upload-widget-remove-button')
-        self.preview_image_upload_input = page.get_by_test_id('create-course-preview-image-upload-widget-input')
-        self.create_course_title_input = page.get_by_test_id('create-course-form-title-input').locator('input')
-        self.create_course_estimated_time_input = page.get_by_test_id('create-course-form-estimated-time-input').locator('input')
-        self.create_course_description_textarea = page.get_by_test_id('create-course-form-description-input').locator('textarea').first
-        self.create_course_max_score_input = page.get_by_test_id('create-course-form-max-score-input').locator('input')
-        self.create_course_min_score_input = page.get_by_test_id('create-course-form-min-score-input').locator('input')
-        self.exercises_title = page.get_by_test_id('create-course-exercises-box-toolbar-title-text')
-        self.create_exercise_button = page.get_by_test_id('create-course-exercises-box-toolbar-create-exercise-button')
-        self.exercises_empty_view_icon = page.get_by_test_id('create-course-exercises-empty-view-icon')
-
-
-    def check_visible_create_course_title(self):
-        expect(self.create_course_title).to_be_visible()
-        expect(self.create_course_title).to_have_text('Create course')
-
-
-    def check_disabled_create_course_button(self):
-        expect(self.create_course_button).to_be_disabled()
-
-
-    def check_visible_image_preview_empty_view(self):
-        expect(self.preview_empty_view_icon).to_be_visible()
-        expect(self.preview_empty_view_title).to_be_visible()
-        expect(self.preview_empty_view_title).to_have_text('No image selected')
-        expect(self.preview_empty_view_description).to_be_visible()
-        expect(self.preview_empty_view_description).to_have_text('Preview of selected image will be displayed here')
-
-
-    def check_visible_image_upload_view(self, is_image_uploaded: bool = False):
-        expect(self.preview_image_upload_icon).to_be_visible()
-        expect(self.preview_image_upload_title).to_be_visible()
-        expect(self.preview_image_upload_title).to_have_text('Tap on "Upload image" button to select file')
-        expect(self.preview_image_upload_description).to_be_visible()
-        expect(self.preview_image_upload_description).to_have_text('Recommended file size 540X300')
-        expect(self.preview_image_upload_button).to_be_visible()
-        if is_image_uploaded:
-            expect(self.preview_image_remove_button).to_be_visible()
-
-
-    def check_visible_create_course_form(
-        self,
-        title: str,
-        description: str,
-        estimated_time: str,
-        max_score: str,
-        min_score: str
-    ):
-        fields = [
-            (self.create_course_title_input, title),
-            (self.create_course_estimated_time_input, estimated_time),
-            (self.create_course_description_textarea, description),
-            (self.create_course_max_score_input, max_score),
-            (self.create_course_min_score_input, min_score),
-        ]
-        for element, expected_value in fields:
-            expect(element).to_be_visible()
-            expect(element).to_have_value(expected_value)
-
-
-    def check_visible_exercises_title(self):
-        expect(self.exercises_title).to_be_visible()
-        expect(self.exercises_title).to_have_text('Exercises')
-
-
-    def check_visible_create_exercise_button(self):
-        expect(self.create_exercise_button).to_be_visible()
-
+        self.navbar = NavbarComponent(page)
+        self.image_upload_widget = ImageUploadWidgetComponent(page, 'create-course-preview')
+        self.exercises_empty_view = EmptyViewComponent(page, 'create-course-exercises')
+        self.create_course_form = CreateCourseFormComponent(page)
+        self.create_course_toolbar_view = CreateCourseToolbarViewComponent(page)
+        self.create_exercise_form = CreateCourseExerciseFormComponent(page)
+        self.create_course_exercises_toolbar_view = CreateCourseExercisesToolbarViewComponent(page)
 
     def check_visible_exercises_empty_view(self):
-        expect(self.exercises_empty_view_icon).to_be_visible()
-
-
-    def upload_preview_image(self, file: str):
-        self.preview_image_upload_input.set_input_files(file)
-
-
-    def fill_create_course_form(self, title: str, estimated_time: str, description: str, max_score: str, min_score: str):
-        fields = [
-            (self.create_course_title_input, title),
-            (self.create_course_estimated_time_input, estimated_time),
-            (self.create_course_description_textarea, description),
-            (self.create_course_max_score_input, max_score),
-            (self.create_course_min_score_input, min_score),
-        ]
-        for element, value in fields:
-            element.fill(value)
-            expect(element).to_have_value(value)
-
-
-    def click_create_course_button(self):
-        self.create_course_button.click()
+        self.exercises_empty_view.check_visible(
+            title='There is no exercises',
+            description='Click on "Create exercise" button to create new exercise'
+        )
